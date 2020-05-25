ceb7a45cbc1a0335da286455109d45fe3732e1f1
Mr. Piya Pimchankam <piya.pimchankam@gmail.com>
2020-05-25 09:27:26 +0700

    Update ros-melodic-ins.sh

--- ros-melodic-ins.sh
+++ ros-melodic-ins.sh
@@ -1,8 +1,8 @@
 set -e
 echo "Setup your sources.list"
-#sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
 echo "Set up your keys"
-#sudo apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
+sudo apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
 echo "Installation"
 
 sudo apt update
