e014bd471a8c1a5898990ec414843ae13e2e643b
Mr. Piya Pimchankam <piya.pimchankam@gmail.com>
2020-05-25 08:57:56 +0700

    Add files via upload

---
+++ ros-melodic-ins.sh
@@ -0,0 +1,19 @@
+set -e
+echo "Setup your sources.list"
+#sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+echo "Set up your keys"
+#sudo apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
+echo "Installation"
+
+sudo apt update
+sudo apt install -y ros-melodic-desktop-full 
+sudo apt install -y ros-melodic-slam-gmapping
+echo "source /opt/ros/melodic/setup.bash" >> ~/.bashrc
+source ~/.bashrc
+
+sudo apt install -y python-rosdep python-rosinstall python-rosinstall-generator python-wstool build-essential
+
+sudo apt install -y python-rosdep
+sudo rosdep init
+rosdep update
+
---
+++ swap-ins.sh
@@ -0,0 +1,33 @@
+#change the mode to MAX-N, issue
+sudo nvpmodel -m 0
+
+set -e
+#install Swapfile
+SWAPDIRECTORY="/mnt"
+# Ubuntu recommends 6GB for 4GB of memory when using suspend
+# You can use 1 or 2 if need be
+SWAPSIZE=6
+echo "Creating Swapfile at: " $SWAPDIRECTORY
+echo "Swapfile Size: " $SWAPSIZE"G"
+#Create a swapfile for Ubuntu at the current directory location
+sudo fallocate -l $SWAPSIZE"G" $SWAPDIRECTORY"/swapfile"
+cd $SWAPDIRECTORY
+#List out the file
+ls -lh swapfile
+# Change permissions so that only root can use it
+sudo chmod 600 swapfile
+#List out the file
+ls -lh swapfile
+#Set up the Linux swap area
+sudo mkswap swapfile
+#Now start using the swapfile
+sudo swapon swapfile
+#Show that it's now being used
+swapon -s
+echo "Modifying /etc/fstab to enable on boot"
+SWAPLOCATION=$SWAPDIRECTORY"/swapfile"
+echo $SWAPLOCATION
+sudo sh -c 'echo "'$SWAPLOCATION' swap swap defaults 0 0" >> /etc/fstab'
+echo "Swap file has been created"
+echo "Reboot to make sure changes are in effect"
+
