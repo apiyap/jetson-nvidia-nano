dbd49869cf4c5998a8eda21cb2343fa7410a4459
Mr. Piya Pimchankam <piya.pimchankam@gmail.com>
2020-06-02 20:16:36 +0700

    Update get-ros.sh

--- get-ros.sh
+++ get-ros.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 set -e
 ROS_DISTRO=melodic
-ROS_PKG=vision_opencv
+ROS_PKG=$1  #vision_opencv
+if [ -z "$ROS_PKG" ] 
+then
+{
+      echo "ROS Package is empty";
+      exit 1 
+}
+else
+      echo "Get Package \$ROS_PKG"
+fi
+
 rosinstall_generator $ROS_PKG --rosdistro $ROS_DISTRO --deps --tar > $ROS_DISTRO-$ROS_PKG.rosinstall
 echo "Get source code.."
 
@@ -14,7 +24,8 @@ else
     wstool update -j4 -t src
 fi
 
-set -echo
+set -e
 
 echo "Resolving Dependencies"
 #rosdep install --from-paths src --ignore-src --rosdistro melodic -y
+catkin_make
